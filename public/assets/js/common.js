904c4815bcd071fdca84fc0351c0d013135fb080
zhangrong <569907163@qq.com>
2020-03-19 16:39:09 +0800

    完成侧边栏登录用户显示对应用户的姓名和头像

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -15,4 +15,19 @@ $('#logout').on('click',function(){
         }
       })     
       }
+  });
+
+  //修改侧边栏登录用户显示对用用户的姓名和头像
+  //用到的路由是根据id查询用户
+  $.ajax({
+    type:'get',
+    url:'/users/' + userId,
+    success:function(res){
+      // console.log(res);
+      
+      //把名字放在侧边栏的名字框中
+      $('.profile h3').text(res.nickName);
+      //把头像放在侧边栏头像框中
+      $('.profile img').prop('src',res.avatar)
+    }
   })
